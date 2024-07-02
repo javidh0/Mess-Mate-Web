db45fd7147581121efa9838d70a135b678b2f1fc
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-07-02 18:32:43 +0530

    home page

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,8 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { RiContactsLine } from "react-icons/ri";
-import { IoMdMenu } from "react-icons/io";
 import "./home.css";
 import "../app.css"
+
+function MenuCard(props) {
+    return (
+        <div className="card">
+            <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2023/06/brown-rice-dosa-recipe.jpg" alt="" />
+            <div>
+                <h4>{props.name}</h4>
+                <p>Overall Rating {props.rate}</p>
+            </div>
+        </div>
+    );
+}
+
+function builder(n) {
+    let tr = [];
+    for(var i=0; i<n; i++) {
+        tr.push(<MenuCard name="Dosa" rate="4.5"/>);
+    }
+    return tr;
+}
+
+function MenuDisplay(props) {
+    return (
+        <div className="menu-display">
+            <h2>Today's Breakfast</h2>
+            <p>Time : 7:00am - 9:00am</p>
+            <div className="Scroll">
+                {builder(10)}
+            </div>
+        </div>
+    )
+}
+
+function MenuEachDay(props) {
+    
+}
 
 function Options(){
     function MouseOverHandle() {
@@ -52,7 +87,21 @@ function Profile(props){
     )
 }
 
+function dayBuilder(day, setDay){
+    let date = new Date();
+    let tr = [];
+    for(var i=0; i<7; i++){
+        tr.push(
+            <div onClick={() => setDay(i)} id={day == i ? "time-select":""}><p>{date.getDay()}</p></div>
+        );
+        date.setDate(date.getDate() + 1);
+    }
+
+    return tr;
+}
+
 function Home(){
+    const [day, setDay] = useState(0);
     const [disOptions, setDisOptions] = useState(false);
 
     function optionsToogle() {
@@ -66,6 +115,11 @@ function Home(){
                 <Profile optionsToogle = {optionsToogle} />
             </nav>
             {disOptions && <Options/>}
+            <div className="time">
+                {dayBuilder(day, setDay)}
+            </div>
+            <MenuDisplay/>
+            
         </div>
     );
 }
