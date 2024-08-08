f5b08187fea36913121e8929e6846ed074c3f6bd
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-08-08 23:06:55 +0530

    Update api.jsx

--- src/home/api.jsx
+++ src/home/api.jsx
@@ -2,7 +2,24 @@ import axios from "axios";
 
 var api_root = "http://localhost:1729/food?"
 
-// headers: {'Authorization': `Bearer ${token}`}
+// headers: {'Authorization': `Bearer ${token}`} food_meal
+
+async function getFoodByDayMeal(day, meal, token){
+    var body = {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Authorization' : `Bearer ${token}`
+        }
+    };
+
+    let api_root_temp = `http://localhost:1729/food_meal?day=${day}&meal=${meal}`; 
+
+    var temp = await axios.get(api_root_temp, body);
+    console.log(temp.data);
+    return temp.data;
+}
+
 
 async function getFoodByDay(day, token){
     var body = {
@@ -31,4 +48,4 @@ async function getFoodById(id, token){
     return temp.data;
 }
 
-export {getFoodByDay, getFoodById};
+export {getFoodByDay, getFoodById, getFoodByDayMeal};
